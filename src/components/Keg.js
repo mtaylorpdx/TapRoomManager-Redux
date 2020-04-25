324e0307ff83685afff615bebd948464281c679c
mtaylorpdx <taylor.matt@protonmail.com>
2020-04-24 22:07:44 -0700

    switch onclick from div to button

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -7,11 +7,10 @@ function Keg(props){
   return (
     <React.Fragment>
       <Card className='kegCard'>
-          <div onClick = {() => props.whenKegClicked(props.id)}>
+          <button onClick = {() => props.whenKegClicked(props.id)}>Details</button>
             <h4>${props.price} {props.name} - {props.brewery}</h4>
             <h5>IBUs: {props.ibu} - ABV: {props.abv}</h5>
             <h5>Pours remaining: {props.quantity}</h5>
-          </div>
       </Card>
     </React.Fragment>
   );
