4195f1a9b29784c184be2cc6d23dde0c732e9e00
mtaylorpdx <taylor.matt@protonmail.com>
2020-05-01 10:24:53 -0700

    add failing test to add new keg data

--- src/_tests_/reducers/tap-list-reducer.test.js
+++ src/_tests_/reducers/tap-list-reducer.test.js
@@ -5,3 +5,64 @@ describe('tapListReducer', () => {
     expect(tapListReducer({}, { type: null })).toEqual({});
     });
 });
+
+describe('tapListReducer', () => {
+
+  let action;
+  const kegData = {
+    name: "All Day IPA",
+    brewery: "Founders",
+    price: "5.00",
+    ibu: "42",
+    abv: "4.7",
+    quantity: 124,
+    id: 1
+  };
+
+  test('Should successfully add new keg data to masterKegList', () => {
+    const { name, brewery, price, ibu, abv, quantity, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brewery: brewery,
+      price: price,
+      ibu: ibu,
+      abv: abv,
+      quantity: quantity,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brewery: brewery,
+        price: price,
+        ibu: ibu,
+        abv: abv,
+        quantity: quantity,
+        id: id
+      }
+    })
+  })
+});
+
+
+
+// {
+//   name: "All Day IPA",
+//   brewery: "Founders",
+//   price: "5.00",
+//   ibu: "42",
+//   abv: "4.7",
+//   quantity: 124,
+//   id: 1
+// },
+// {
+//   name: "Blackstone",
+//   brewery: "Driftwood Brewing",
+//   price: "6.00",
+//   ibu: "20",
+//   abv: "6",
+//   quantity: 124,
+//   id: 2
+// }
